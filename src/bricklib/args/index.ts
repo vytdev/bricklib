b947742f94b27518f2c9a71334c9829386bdd282
Vincent Yanzee J. Tan <vytdev@gmail.com>
2025-05-29 18:09:49 +0800

    add more arg types

--- src/bricklib/args/index.ts
+++ src/bricklib/args/index.ts
@@ -1,4 +1,4 @@
 export type * from './defs.ts';
 export * from './parser.js';
 export * from './tokens.js';
-export * from './types.js';
+export * as parsers from './types.js';
--- src/bricklib/args/types.ts
+++ src/bricklib/args/types.ts
@@ -1,28 +1,68 @@
-import type { CmdArgument } from './defs.ts';
+import type { CmdArgument, TypeParser } from './defs.ts';
 import { ArgTokenStream } from './tokens.js';
 
-export const parsers = {
-  'string': (_: CmdArgument, args: ArgTokenStream) => {
+export function string(): TypeParser<string>
+{
+  return (_: CmdArgument, args: ArgTokenStream): string => {
     const arg = args.current;
     args.consume();
     return arg;
-  },
+  };
+}
 
-  'number': (_: CmdArgument, args: ArgTokenStream) => {
+export function float(): TypeParser<number>
+{
+  return (_: CmdArgument, args: ArgTokenStream): number => {
     const arg = args.current;
     args.consume();
-    /* try parse number */
+    /* try parse float */
     const val = parseFloat(arg);
     if (isNaN(val))
-      throw 'not-a-number: ' + arg;
+      throw 'invalid float: ' + arg;
     return val;
-  },
+  };
+}
 
-  'boolean': (_: CmdArgument, args: ArgTokenStream) => {
+export function bool(): TypeParser<boolean>
+{
+  return (_: CmdArgument, args: ArgTokenStream): boolean => {
     const arg = args.current;
     args.consume();
     if (arg == 'true')  return true;
     if (arg == 'false') return false;
     throw 'invalid boolean: ' + arg;
-  }
-};
+  };
+}
+
+export function int(): TypeParser<number>
+{
+  return (_: CmdArgument, args: ArgTokenStream): number => {
+    const arg = args.current;
+    args.consume();
+    const val = parseInt(arg);
+    if (isNaN(val))
+      throw 'invalid integer: ' + arg;
+    return val;
+  };
+}
+
+export function variadic<T>(fn: TypeParser<T>): TypeParser<T[]>
+{
+  return (def: CmdArgument, args: ArgTokenStream): T[] => {
+    const vals = [];
+    while (!args.isEnd)
+      vals.push(fn(def, args));
+    return vals;
+  };
+}
+
+export function strenum<T extends string>(vals: T[]): TypeParser<T>
+{
+  return (_: CmdArgument, args: ArgTokenStream): T => {
+    const arg = args.current as T;
+    args.consume();
+    if (!vals.includes(arg))
+      throw 'expected any of: ' + vals.join(', ') + '. got: ' + arg;
+    return arg;
+  };
+}
